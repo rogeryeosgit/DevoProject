74616305b7fa52c52ffd565e90b206304505f183
RogerY <roger@geekshacking.com>
2020-04-04 17:45:33 +0800

    moved handburger to the right and started on auth

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app :dark="setTheme">
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" fixed temporary app>
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" fixed temporary app right>
       <v-list>
         <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
@@ -13,9 +13,9 @@
       </v-list>
     </v-navigation-drawer>
     <v-app-bar fixed app color="indigo" dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
       <v-spacer />
+      <v-app-bar-nav-icon right @click.stop="drawer = !drawer" />
     </v-app-bar>
     <v-content>
       <v-container>
@@ -42,8 +42,8 @@ export default {
         },
         {
           icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire"
+          title: "Login",
+          to: "/auth"
         }
       ],
       miniVariant: false,
@@ -52,7 +52,7 @@ export default {
   },
   computed: {
     setTheme() {
-      return this.$vuetify.theme.dark = false
+      return (this.$vuetify.theme.dark = false);
     }
   }
 };
