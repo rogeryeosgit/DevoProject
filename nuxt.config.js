0b9fe5d46ae8ddcf964bd4c966c1a45fe2139914
RogerY <roger@geekshacking.com>
2020-08-24 23:56:11 +0800

    fixing localhost

--- nuxt.config.js
+++ nuxt.config.js
@@ -69,7 +69,7 @@ module.exports = {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {
-    baseURL: process.env.BASE_URL, //|| 'http://localhost:3000',
+    baseURL: process.env.BASE_URL || 'http://localhost:3000',
     credentials: false,
     https: true
   },
