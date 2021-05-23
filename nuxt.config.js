97ac306576f672aa683f9de22889d138b6f9da67
RogerY <roger@geekshacking.com>
2021-05-23 23:35:48 +0800

    fixing env bug

--- nuxt.config.js
+++ nuxt.config.js
@@ -94,6 +94,20 @@ module.exports = {
   publicRuntimeConfig: {
     googleAnalytics: {
       id: process.env.GA_ID
+    },
+    firebase: {
+      config: {
+        apiKey: process.env.FB_KEY,
+        authDomain: process.env.FB_AUTH_DOMAIN,
+        databaseURL: process.env.FB_DATABASE_URL,
+        projectId: process.env.FB_PROJECT_ID,
+        storageBucket: process.env.FB_STORAGE_BUCKET,
+        messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+        appId: process.env.FB_APP_ID
+      },
+      services: {
+        auth: true
+      }
     }
   },
   firebase: {
